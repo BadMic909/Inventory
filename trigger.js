53611635950244deed4a4db4eb404ea02cce769f
BadMic909 <badmic909@gmail.com>
2026-03-02 10:51:06 +0530

    Update URL construction to include additional key

--- trigger.js
+++ trigger.js
@@ -4,7 +4,8 @@ const { chromium } = require('playwright');
   const browser = await chromium.launch();
   const page = await browser.newPage();
   const key = process.env.MY_REPORT_KEY;
-  const url = `https://badmic909.github.io/Inventory/post.html?key=${key}`;
+  const key2 = process.env.k2;
+  const url = `https://badmic909.github.io/Inventory/post.html?key=${key}?k2=${key2}`;
   
   console.log("Opening page...");
   await page.goto(url);
